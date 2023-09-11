ede34577ca97587cf0d3b6c921160876275f3e7c
Jefersonz19 <jefersonz1997@gmail.com>
2023-09-11 07:53:28 -0500

    Exercise_5

---
+++ Exercise_5/Program.cs
@@ -0,0 +1,46 @@
+﻿//Exercise 5
+
+class Week
+{
+    static void Main() {
+
+    int n;
+    do {
+    Console.WriteLine("Ingrese un número del 1 al 7");
+    n = Convert.ToInt32(Console.ReadLine());
+
+    }  while (n < 1     || n >7 );
+    
+    
+    string dia = " ";
+
+    switch(n){
+        case 1:
+        dia = "Lunes";
+        break;
+
+        case 2:
+        dia = "Martes";
+        break;
+
+        case 3:
+        dia = "Miercoles";
+        break;
+
+        case 4:
+        dia = "Jueves";
+        break;
+
+        case 5:
+        dia = "Viernes";
+        break;
+
+        default:
+        dia = "Numero fuera del rango laboral";
+        break;
+    }
+
+     Console.WriteLine($"Resultado: {dia}");
+    }
+
+}
