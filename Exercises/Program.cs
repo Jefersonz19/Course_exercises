6476b3016bb05f8914d355512976e9af32ea1b32
Jefersonz19 <jefersonz1997@gmail.com>
2023-09-10 19:29:02 -0500

    First Exercise solved

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,2 +1,25 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿//Exercise 1
+class Program
+{
+
+    static void Main()
+    {
+
+        Console.WriteLine("Ingrese un número ");
+        double a = double.Parse(Console.ReadLine());
+        
+        if (a > 0)
+        {
+            a = Math.Pow(a, 2);
+            Console.WriteLine($"Resultado: El número elevado al cuadrado es: {a}");
+        }
+        else if (a < 0)
+        {
+            Console.WriteLine("Resultado: Numero negativo");
+        }
+        else
+        {
+            Console.WriteLine("Resultado: 0");
+        }
+    }
+}
