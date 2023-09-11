82ed729938f8658afefa601b1b450826965af9ae
Jefersonz19 <jefersonz1997@gmail.com>
2023-09-10 22:32:40 -0500

    Exercise_2 solved

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Exercise 2
+class Program
+{
+
+    static void Main()
+    {
+
+        Console.WriteLine("Ingrese el primer número ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo número ");
+        double b = double.Parse(Console.ReadLine());
+        
+        if (a > b)
+        {
+            Console.WriteLine($"Resultado: {a*2}");
+        }
+        else
+        {
+            Console.WriteLine($"Resultado: {b*3}");
+        }
+    }
+}
