13f446431c782f3244e9e683e3f526d6a5e5b9b3
Jefersonz19 <jefersonz1997@gmail.com>
2023-09-10 23:04:54 -0500

    Exercise 3 solved

---
+++ Exercise_3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Exercise 3
+class Program
+{
+
+    static void Main()
+    {
+
+        Console.WriteLine("Ingrese un número ");
+        double a = double.Parse(Console.ReadLine());
+        
+        if (a > 0)
+        {
+            a = Math.Sqrt(a);
+            Console.WriteLine($"Resultado: {a}");
+        }
+        else
+        {
+            a = Math.Pow(a,2);
+            Console.WriteLine($"Resultado: {a}");
+        }
+    }
+}
