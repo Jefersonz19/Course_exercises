ef11da26bca30e831afe38312ef3febcc599864a
Jefersonz19 <jefersonz1997@gmail.com>
2023-09-11 07:26:12 -0500

    Exercise_4 done

---
+++ Exercise_4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Exercise 3
+
+
+    var  circle1 = new Circle();
+    Console.WriteLine("Ingrese el radio de un circulo");
+    var r = double.Parse(Console.ReadLine());
+    circle1.radius = r;
+    Console.WriteLine($"The perimeter is: {circle1.Perimeter()}");
+    
+class Circle
+{
+    public double radius { get; set; }
+
+    public double Perimeter() => 2*System.Math.PI * radius;
+ 
+}
